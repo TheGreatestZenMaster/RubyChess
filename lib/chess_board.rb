cc97735348f36404620eb5eb2bfd29a12bd61356
TheGreatestZenMaster <jetloose@gmail.com>
2016-10-27 04:50:00 +0000

    added starting positions

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -12,6 +12,8 @@ class ChessBoard
                                     :rook => "\u2656".encode('utf-8').red, :bishop => "\u2657".encode('utf-8').red, 
                                     :knight => "\u265B".encode('utf-8').red, :pawn => "\u2659".encode('utf-8').red})
         @space = Hash(:white => "\u2652".encode('utf-8').white, :black => "\u2652".encode('utf-8').black)
+        starting_positions
+        display
     end
     
     def build_board(cols, rows)
@@ -22,6 +24,31 @@ class ChessBoard
             end
         end
     end
+    
+    def starting_positions
+        (1..8).each do |column|
+            @squares[[column, 2]] = @pieces[:white][:pawn]
+            @squares[[column, 7]] = @pieces[:black][:pawn]
+            if column == 1 || column == 8
+                @squares[[column, 1]] = @pieces[:white][:rook]
+                @squares[[column, 8]] = @pieces[:black][:rook]
+            elsif column == 2 || column == 7
+                @squares[[column, 1]] = @pieces[:white][:knight]
+                @squares[[column, 8]] = @pieces[:black][:knight]
+            elsif column == 3 || column == 6
+                @squares[[column, 1]] = @pieces[:white][:bishop]
+                @squares[[column, 8]] = @pieces[:black][:bishop]
+            elsif column == 4
+                @squares[[column, 1]] = @pieces[:white][:queen]
+                @squares[[column, 8]] = @pieces[:black][:king]
+            else
+                @squares[[column, 1]] = @pieces[:white][:king]
+                @squares[[column, 8]] = @pieces[:black][:queen]
+            end
+        end
+        
+    end
+        
     
     def piece_output(column,row, color)
         square = @squares[[column,row]]
@@ -56,26 +83,3 @@ end
 
 
 chess = ChessBoard.new
-chess.squares[[2,2]] = chess.pieces[:black][:rook]
-puts chess.squares.inspect
-chess.display
-
-
-
-
-
-
-puts ""
-puts ""
-
-puts space[:white]*3 + space[:black]*3 + space[:white]*3
-puts (space[:white] + "#{pieces[:white][:king]}" + space[:white]) + space[:black] +"#{pieces[:black][:knight]}" + space[:black] + space[:white]*3
-puts space[:white]*3 + space[:black]*3 + space[:white]*3
-
-puts space[:black]*3 + space[:white]*3 + space[:black]*3 
-puts space[:black] + "#{pieces[:white][:bishop]}" + space[:black] + space[:white]*3 + space[:black]*3 
-puts space[:black]*3 + space[:white]*3 + space[:black]*3
-
-puts space[:white]*3 + space[:black]*3 + space[:white]*3
-puts space[:white]*3 + space[:black]*3 + space[:white]*3
-puts space[:white]*3 + space[:black]*3 + space[:white]*3
