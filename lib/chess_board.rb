556cf2bc9a4b80776bcfc117d40d4e8a85df1254
TheGreatestZenMaster <jetloose@gmail.com>
2016-10-27 04:23:22 +0000

    Add display method

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -1,44 +1,69 @@
 require 'colorize'
 
 class ChessBoard
-    attr_reader :squares
+    attr_reader :squares, :pieces, :space
     
     def initialize
         build_board(8,8)
+        @pieces = Hash( :white => { :king => "\u2654".encode('utf-8').white, :queen => "\u2655".encode('utf-8').white, 
+                                    :rook => "\u2656".encode('utf-8').white, :bishop => "\u2657".encode('utf-8').white, 
+                                    :knight => "\u265B".encode('utf-8').white, :pawn => "\u2659".encode('utf-8').white}, 
+                        :black => {:king => "\u2654".encode('utf-8').red, :queen => "\u2655".encode('utf-8').red, 
+                                    :rook => "\u2656".encode('utf-8').red, :bishop => "\u2657".encode('utf-8').red, 
+                                    :knight => "\u265B".encode('utf-8').red, :pawn => "\u2659".encode('utf-8').red})
+        @space = Hash(:white => "\u2652".encode('utf-8').white, :black => "\u2652".encode('utf-8').black)
     end
     
     def build_board(cols, rows)
-        @squares = Array.new
+        @squares = Hash.new
         (1..cols).each do |column|
             (1..rows).each do |row|
-                @squares << [column,row]
+                @squares[[column,row]] = nil
+            end
+        end
+    end
+    
+    def piece_output(column,row, color)
+        square = @squares[[column,row]]
+        square.nil?? @space[color.to_sym]: square
+    end
+    
+    def display
+        8.downto(1).each do |row|
+            (1..3).each do |section|
+                row_output = ""
+                (1..8).each do |column|
+                    if row % 2 == 0 
+                        if column % 2 == 1
+                            (section % 2 == 1)? row_output << @space[:white]*3: row_output << @space[:white] + "#{piece_output(column,row,"white")}" + @space[:white]
+                        elsif column % 2 == 0
+                            (section % 2 == 1)? row_output << @space[:black]*3: row_output << @space[:black] + "#{piece_output(column,row,"black")}" + @space[:black]
+                        end
+                    elsif row % 2 == 1 && 
+                        if column % 2 == 1
+                            (section % 2 == 1)? row_output << @space[:black]*3: row_output << @space[:black] + "#{piece_output(column,row,"black")}" + @space[:black]
+                        elsif column % 2 == 0
+                            (section % 2 == 1)? row_output << @space[:white]*3: row_output << @space[:white] + "#{piece_output(column,row,"white")}" + @space[:white]
+                        end
+                    end
+                end
+            puts row_output
             end
         end
     end
 end
 
-pieces = Hash(  :white => { :king => "\u2654".encode('utf-8').white, :queen => "\u2655".encode('utf-8').white, 
-                            :rook => "\u2656".encode('utf-8').white, :bishop => "\u2657".encode('utf-8').white, 
-                            :knight => "\u265B".encode('utf-8').white, :pawn => "\u2659".encode('utf-8').white}, 
-                :black => {:king => "\u2654".encode('utf-8').red, :queen => "\u2655".encode('utf-8').red, 
-                            :rook => "\u2656".encode('utf-8').red, :bishop => "\u2657".encode('utf-8').red, 
-                            :knight => "\u265B".encode('utf-8').red, :pawn => "\u2659".encode('utf-8').red})
-space = Hash(:white => "\u2652".encode('utf-8').white, :black => "\u2652".encode('utf-8').black)
+
 
 chess = ChessBoard.new
+chess.squares[[2,2]] = chess.pieces[:black][:rook]
 puts chess.squares.inspect
+chess.display
 
-(1..8).each do |rows|
-    if rows % 2 == 0
-        puts (space[:white]*3 + space[:black]*3) * 4
-        puts (space[:white]*3 + space[:black]*3) * 4
-        puts (space[:white]*3 + space[:black]*3) * 4
-    else
-        puts (space[:black]*3 + space[:white]*3) * 4
-        puts (space[:black]*3 + space[:white]*3) * 4
-        puts (space[:black]*3 + space[:white]*3) * 4
-    end
-end
+
+
+
+
 
 puts ""
 puts ""
